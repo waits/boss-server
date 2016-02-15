963bbec7a2d99ee29778e4e3ccc74a35cf9404e9
Dylan Waits <dylan@waits.io>
2016-02-15 14:02:23 -0800

    Support deploying multiple repositories

--- boss.rb
+++ boss.rb
@@ -9,19 +9,18 @@ post '/events' do
 end
 
 def deploy
-  config = YAML.load File.read('config/boss.yml')
-  Net::SSH.start(config['host'], 'boss', :keys => [config['key']]) do |ssh|
-    body = request.body.read
-    if body.length > 0
-      data = JSON.parse body
-      if data['ref'] == 'refs/heads/master' and !data['forced'] and data['commits'].length > 0
-        output = ssh.exec! "cd #{config['path']} && git pull origin master && rake assets:precompile && unicornd upgrade"
-        if output['Upgrade Complete'] then [200, 'Deploy completed.'] else [500, 'Deploy failed.'] end
-      else
-        [422, 'Payload isn\'t for master or doesn\'t contain any commits.']
-      end
+  body = request.body.read
+#   return [400, 'Payload was empty.'] unless body.size > 0
+  data = JSON.parse(body)
+  repos = YAML.load(File.read('config/repos.yml'))
+  config = repos[data['repository']['name']]
+  Net::SSH.start(config['host'], config['user'], :keys => [config['key']]) do |ssh|
+    if data['ref'] == 'refs/heads/master' and !data['forced'] and data['commits'].length > 0
+      output = ssh.exec! "cd #{config['path']} && git pull origin master && bin/rake assets:precompile && unicornd upgrade"
+      puts output
+      if output['Upgrade Complete'] then [200, 'Deploy completed.'] else [500, 'Deploy failed.'] end
     else
-      [400, 'Payload was empty.']
+      [422, 'Payload isn\'t for master or doesn\'t contain any commits.']
     end
   end
 end
